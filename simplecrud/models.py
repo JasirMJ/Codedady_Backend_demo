bbdd79a1a3dfdb31c5f5da1c0ccdfed38536bdd3
thanseerthansi <thanseermedammal720@gmail.com>
2022-03-03 15:18:16 +0530

    model only

--- simplecrud/models.py
+++ simplecrud/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 
+
 class ProductsModel(models.Model):
     name = models.CharField(max_length=50,null=False,blank=False)
     price = models.FloatField(default=0.0)
